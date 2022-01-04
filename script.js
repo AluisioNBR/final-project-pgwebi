44ce465c0a6ace3f56c8220b903fffa743e415a0
AluisioNBR <lulu70638@gmail.com>
2022-01-04 19:40:20 -0300

    index 100% | contact 100%

--- script.js
+++ script.js
@@ -2,16 +2,35 @@ $(document).ready(function () {
     var Responsive = {
         resize: function () {
             if (window.innerWidth <= 720) {
-                $('#float-menu').removeClass('active');
-                $('#float-menu').addClass('not-active');
+                Responsive.desactiveMenu();
+                Responsive.activeMenuButton();
             }
-            else if (window.innerWidth > 720) {
+            else {
                 $('#float-menu').removeClass('not-active');
                 $('#float-menu').addClass('active');
+                Responsive.desactiveMenuButton();
             }
+        },
+        desactiveMenu: function () {
+            $('#float-menu').removeClass('active');
+            $('#float-menu').addClass('not-active');
+        },
+        activeMenuButton: function () {
+            $('#float-button').removeClass('not-active');
+            $('#float-button').addClass('active');
+        },
+        desactiveMenuButton: function () {
+            $('#float-button').removeClass('active');
+            $('#float-button').addClass('not-active');
         }
     };
-    if (window.innerWidth <= 720)
-        $('#float-menu').addClass('not-active');
+    if (screen.width <= 720 || window.innerWidth <= 720) {
+        Responsive.desactiveMenu();
+        Responsive.activeMenuButton();
+    }
     window.onresize = Responsive.resize;
+    $('#float-button').on('click', function () {
+        $('#float-menu').toggleClass('active');
+        $('#float-menu').toggleClass('not-active');
+    });
 });
--- script.ts
+++ script.ts
@@ -2,15 +2,40 @@ $(document).ready(function(){
     const Responsive = {
         resize(){
             if(window.innerWidth <= 720) {
-                $('#float-menu').removeClass('active')
-                $('#float-menu').addClass('not-active')
+                Responsive.desactiveMenu()
+                Responsive.activeMenuButton()
             }
-            else if(window.innerWidth > 720) {
+            else {
                 $('#float-menu').removeClass('not-active')
                 $('#float-menu').addClass('active')
+                Responsive.desactiveMenuButton()
             }
+        },
+
+        desactiveMenu(){
+            $('#float-menu').removeClass('active')
+            $('#float-menu').addClass('not-active')
+        },
+
+        activeMenuButton(){
+            $('#float-button').removeClass('not-active')
+            $('#float-button').addClass('active')
+        },
+
+        desactiveMenuButton(){
+            $('#float-button').removeClass('active')
+            $('#float-button').addClass('not-active')
         }
     }
-    if(window.innerWidth <= 720) $('#float-menu').addClass('not-active')
+    
+    if(screen.width <= 720 || window.innerWidth <= 720) {
+        Responsive.desactiveMenu()
+        Responsive.activeMenuButton()
+    
+    }
     window.onresize = Responsive.resize
+    $('#float-button').on('click', function (){
+        $('#float-menu').toggleClass('active')
+        $('#float-menu').toggleClass('not-active')
+    })
 })
