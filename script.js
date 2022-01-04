f2a82d4c370cae8ccb7c918d382f751171d1fbeb
AluisioNBR <lulu70638@gmail.com>
2022-01-03 22:10:24 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    var Responsive = {
+        resize: function () {
+            if (window.innerWidth <= 720 && !$('#float-menu').hasClass('active')) {
+                $('#float-menu').removeClass('active');
+                $('#float-menu').addClass('not-active');
+            }
+            else if (window.innerWidth > 720) {
+                console.log(window.innerWidth);
+                $('#float-menu').removeClass('not-active');
+                $('#float-menu').addClass('active');
+            }
+        }
+    };
+    if (window.innerWidth <= 720)
+        $('#float-menu').addClass('not-active');
+    window.onresize = Responsive.resize;
+});
