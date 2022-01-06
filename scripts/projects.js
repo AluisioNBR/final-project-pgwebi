e247d2f8aafaf3db04e00fbb52fa8487414f90f0
AluisioNBR <lulu70638@gmail.com>
2022-01-06 12:04:07 -0300

    projects.html 90%

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,24 +1,29 @@
 $(document).ready(function () {
     var Projects = {
+        atv2_1: {
+            name: 'Reconstruindo modelos(HTML/CSS)',
+            img: '../img/projects/atv-2.1.jpeg',
+            description: 'Primeira atividade para fixação dos conhecimentos sobre HTML e CSS'
+        },
+        atv2_3: {
+            name: 'Praticando JS',
+            img: '../img/projects/atv-2.3.jpeg',
+            description: 'Primeira atividade para fixação dos conhecimentos sobre JavaScript'
+        },
+        atv3_1: {
+            name: 'Aprendendo jQuery',
+            img: '../img/projects/atv-3.1.jpeg',
+            description: 'Atividade para fixação dos conhecimentos sobre jQuery'
+        },
+        atv3_2: {
+            name: 'Jogo da Velha',
+            img: '../img/projects/atv-3.2.jpeg',
+            description: 'Recriação de um jogo da velha a partir dos novos conhecimentos de jQuery'
+        },
         amazonCopy: {
             name: 'Copia da Amazon',
             img: '../img/projects/amazon_copy.jpeg',
-            description: ''
-        },
-        teste2: {
-            name: 'Projeto 2',
-            img: '../img/projects/amazon_copy.jpeg',
-            description: ''
-        },
-        teste3: {
-            name: 'Projeto 3',
-            img: '../img/projects/amazon_copy.jpeg',
-            description: ''
-        },
-        teste4: {
-            name: 'Projeto 4',
-            img: '../img/projects/amazon_copy.jpeg',
-            description: ''
+            description: 'Primeira avaliação envolvendo nossos conhecimentos sobre HTML e CSS'
         }
     };
     var cont = 0, columns = '1fr';
@@ -32,7 +37,7 @@ $(document).ready(function () {
     var i = 1;
     var _loop_1 = function (project) {
         var newProject = $('<div />').addClass('project').append($('<img />').attr('title', "" + Projects["" + project].name).attr('alt', "" + Projects["" + project].name).attr('src', "" + Projects["" + project].img));
-        var details = $('<div />').attr('id', 'project__details').addClass('not-active').append($('<h3 />').text("" + Projects["" + project].name)).append($('<h4 />').text('Descrição:')).append($('<p />').text("" + Projects["" + project].description));
+        var details = $('<div />').attr('id', 'project__details').addClass('not-active').append($('<h3 />').text("" + Projects["" + project].name)).append($('<p />').text("" + Projects["" + project].description));
         $('#projects-conteiner').append(newProject.append(details));
         newProject.on('mouseover', function () {
             details.removeClass('not-active');
